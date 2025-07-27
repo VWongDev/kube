79e055bd10397b6e14486f6988117cda18fc5488
Vincent Wong <vincent@vwong.dev>
2025-07-27 23:42:42 +1000

    feat(aws): Grant cluster access to admin user

--- aws/init/install-access-role.sh
+++ aws/init/install-access-role.sh
@@ -5,3 +5,10 @@ eksctl create iamidentitymapping \
     --arn arn:aws:iam::${AWS_ACCOUNT}:role/ClusterAccessRole \
     --username eks-general-access \
     --group system:masters
+
+eksctl create iamidentitymapping \
+    --cluster kube-cluster \
+    --region ${AWS_REGION} \
+    --arn arn:aws:iam::${AWS_ACCOUNT}:user/admin \
+    --username eks-admin-access \
+    --group system:masters
