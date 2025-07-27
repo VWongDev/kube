37dbcb94391754cdc8eaca4528df870b08e75a05
Vincent Wong <vincent@vwong.dev>
2025-07-27 22:27:32 +1000

    test

--- aws/init/install-access-role.sh
+++ aws/init/install-access-role.sh
@@ -3,5 +3,12 @@ eksctl create iamidentitymapping \
     --cluster kube-cluster \
     --region ${AWS_REGION} \
     --arn arn:aws:iam::${AWS_ACCOUNT}:role/ClusterAccessRole \
-    --username eks-access \
+    --username eks-general-access \
+    --group system:masters
+
+eksctl create iamidentitymapping \
+    --cluster kube-cluster \
+    --region ${AWS_REGION} \
+    --arn arn:aws:iam::${AWS_ACCOUNT}:user/admin \
+    --username eks-admin-access \
     --group system:masters
