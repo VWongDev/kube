180f86096219cb9a8477b135099457bb8683a847
Vincent Wong <vincent@vwong.dev>
2025-10-27 22:57:15 +1100

    fix(aws): Scale up EKS node group

--- aws/modules/eks/main.tf
+++ aws/modules/eks/main.tf
@@ -38,8 +38,8 @@ resource "aws_eks_node_group" "node_group" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
+    desired_size = 3
+    max_size     = 5
     min_size     = 1
   }
 
